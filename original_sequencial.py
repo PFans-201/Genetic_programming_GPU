ae3d7cd0d2ecafca31b9abc0452c3d1487fc37f2
PFans-201 <pedro.fanica@gmail.com>
2025-12-15 17:30:25 +0000

    Initial commit of genetic programming project

---
+++ original_sequencial.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pandas as pd
+
+
+df = pd.read_csv("data.csv")
+
+funs = [ line.strip() for line in open("functions.txt").readlines() ]
+
+def score(line):
+    for u in ["sinf", "cosf", "tanf", "sqrtf", "expf"]:
+        line = line.replace(u, f"np.{u[:-1]}")
+    for c in df.columns:
+        line = line.replace(f"_{c}_", f"(df[\"{c}\"].values)")
+    a = eval(line)
+    b = df["y"]
+    e = np.square(np.subtract(a, b)).mean()
+    return e
+
+l = funs[0]
+print(score(l), l)
+
+r = min([ (score(line), line) for line in funs ])
+print(f"{r[0]} {r[1]}")
+
+# this is causing issues: <string>:1: RuntimeWarning: invalid value encountered in sqrt
+# 0.060574792608106345 sqrtf((a) + ((c) + (sqrtf(i))))
